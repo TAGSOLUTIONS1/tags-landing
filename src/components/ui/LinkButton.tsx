e5c6c8616e8249a497488d41344100552aa5f235
fatimasalman12 <fatima.salman.work@gmail.com>
2025-04-12 00:30:29 +0500

    Aboutus Page done

--- src/components/ui/LinkButton.tsx
+++ src/components/ui/LinkButton.tsx
@@ -4,18 +4,17 @@ interface LinkbuttonProps {
   title: string;
   href: string;
 }
+
 const Linkbutton = (props: LinkbuttonProps) => {
   return (
-    <>
-      <a
-        href={props.href}
-        className="bg-orange-primary text-white px-5 py-4 rounded-lg hover:bg-orange-600"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        {props.title}
-      </a>
-    </>
+    <a
+      href={props.href}
+      className="bg-orange-primary text-white text-sm sm:text-base px-4 sm:px-5 py-3 sm:py-4 rounded-lg hover:bg-orange-600 transition duration-300 inline-block text-center"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      {props.title}
+    </a>
   );
 };
 
