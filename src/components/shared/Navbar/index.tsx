43d653f2d86005ed099f8ea99c8cb065aab1a715
fatimasalman12 <fatima.salman.work@gmail.com>
2025-04-15 16:52:20 +0500

    Mobile Navbar beautify

--- src/components/shared/Navbar/index.tsx
+++ src/components/shared/Navbar/index.tsx
@@ -6,6 +6,12 @@ import Button from '@/components/ui/Button';
 import Navbutton from '@/components/ui/Navbutton';
 import Link from 'next/link';
 import { HiOutlineMenu, HiOutlineX } from 'react-icons/hi';
+import { RiHome2Line } from 'react-icons/ri';
+import { MdOutlineBusinessCenter } from 'react-icons/md';
+import { BsFileEarmarkBarGraph } from 'react-icons/bs';
+import { PiArticleMediumLight } from 'react-icons/pi';
+import { LuBriefcase } from 'react-icons/lu';
+import { IoIosPeople } from 'react-icons/io';
 
 const Navbar: React.FC = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
@@ -76,13 +82,34 @@ const Navbar: React.FC = () => {
             </button>
           </div>
 
+          <hr className="bg-dark-secondary w-3/4 ml-[35px] mt-[-10px] mb-[20px]" />
+
           {/* Mobile Nav Links */}
-          <Navbutton title="Home" href="/" />
-          <Navbutton title="Industries" href="/#industries" />
-          <Navbutton title="Case Studies" href="/case-studies" />
-          <Navbutton title="Blogs" href="/blogs" />
-          <Navbutton title="Careers" href="/careers" />
-          <Navbutton title="About Us" href="/about-us" />
+          <div className="flex">
+            <RiHome2Line className="ml-3 mt-[18px] h-[22px] w-[22px] text-dark-secondary hover:text-orange-primary" />
+            <Navbutton title="Home" href="/" />
+          </div>
+          <div className="flex">
+            <MdOutlineBusinessCenter className="ml-3 mt-[18px] h-[22px] w-[22px] text-dark-secondary hover:text-orange-primary" />
+            <Navbutton title="Industries" href="/industry" />
+          </div>
+          <div className="flex">
+            <BsFileEarmarkBarGraph className="ml-3 mt-[18px] h-[22px] w-[22px] text-dark-secondary hover:text-orange-primary" />
+            <Navbutton title="Case Studies" href="/case-studies" />
+          </div>
+          <div className="flex">
+            <PiArticleMediumLight className="ml-3 mt-[18px] h-[22px] w-[22px] text-dark-secondary hover:text-orange-primary" />
+            <Navbutton title="Blogs" href="/blogs" />
+          </div>
+          <div className="flex">
+            <LuBriefcase className="ml-3 mt-[18px] h-[22px] w-[22px] text-dark-secondary hover:text-orange-primary" />
+            <Navbutton title="Careers" href="/careers" />
+          </div>
+          <div className="flex">
+            <IoIosPeople className="ml-3 mt-[18px] h-[22px] w-[22px] text-dark-secondary hover:text-orange-primary" />
+            <Navbutton title="About Us" href="/about-us" />
+          </div>
+
           <Link href="/contact-us">
             <Button title="Contact Us" />
           </Link>
